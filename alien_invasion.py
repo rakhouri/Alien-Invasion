5ce76717bf5e2310b8b6697a2b88788d10b8e1e8
rakhouri <richie.khouri@gmail.com>
2023-06-20 20:18:41 -0400

    Added the ability to move right continuously

--- alien_invasion.py
+++ alien_invasion.py
@@ -24,12 +24,13 @@ class AlienInvasion:
         self.ship = Ship(self)
 
         # Set the background color.
-        self.bg_color = (230, 230, 230)
+        self.bg_color = self.settings.bg_color
 
     def run_game(self):
         """Start the main loop for the game"""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
             # Sets the framerate to 60.
             self.clock.tick(60)
@@ -39,6 +40,12 @@ class AlienInvasion:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
     
     def _update_screen(self):
         """Update images on the screen, and flip to the new screen."""
